10855deadea4c4b8d8d579efdeee73da862295de
k9845 <khatririshi2430@gmail.com>
2025-01-16 18:52:42 +0545

    Annotate api in the login url

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -25,7 +25,7 @@ const Login: React.FC = () => {
     e.preventDefault();
     setErrorMessage('');
     try {
-      const response = await axios.post<{ token: string }>(`${import.meta.env.VITE_REACT_APP_API_URL}/login/`, formData);
+      const response = await axios.post<{ token: string }>(`${import.meta.env.VITE_REACT_APP_API_URL}/api/login/`, formData);
       const { token } = response.data;
 
       localStorage.setItem('token', token);
